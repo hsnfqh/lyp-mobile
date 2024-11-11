d420bf7603fddcf5f11fccebe8524c75f64e91e6
Ahmad Hasan Faqih <hasanfaqih@apps.ipb.ac.id>
2024-11-11 13:25:54 +0700

    second commit

---
+++ app/src/main/java/com/example/lyp_marketplace/MarketplaceLypActivity.java
@@ -0,0 +1,24 @@
+package com.example.lyp_marketplace;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MarketplaceLypActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.marketplace_lyp);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
